40b597a9ccccfde99b3351ef1dd0fa6a6b354de6
brunovillero <brunovillero66@gmail.com>
2020-08-30 21:37:00 -0300

    first commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Library;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person("John Doe", "1.234.567-2");
+            Person jane = new Person("Jane Doe", "8.765.432-7");
+            john.IntroduceYourself();
+            jane.IntroduceYourself();
+        }
+    }
+}
