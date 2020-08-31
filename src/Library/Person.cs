5345925f090ef775782e135347027d9340363627
brunovillero <brunovillero66@gmail.com>
2020-08-30 21:37:27 -0300

    first commit

---
+++ src/Library/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Library
+{
+    public class Person 
+    { 
+        private string name; 
+
+        private string id; 
+
+        public Person(string name, string id) 
+        { 
+            this.Name = name; 
+            this.Id = id; 
+        } 
+
+        public void IntroduceYourself() 
+        { 
+            Console.WriteLine($"Soy {this.Name} y mi cédula es {this.Id}"); 
+        }
+
+        public string Id
+        {
+            get 
+            {
+                return this.id;
+            }
+
+            set
+            {
+                if(IdUtils.IdIsValid(value))
+                {
+                    this.id = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un Id válido");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if(value != ""){
+                    this.name = value;
+                }
+            }
+        }
+    }
+}
